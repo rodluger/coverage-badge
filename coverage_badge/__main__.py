0dd01075dfb30b3801c65460fd9afa304da8bd52
Rodrigo Luger <rodluger@gmail.com>
2019-10-31 19:20:44 -0400

    custom label

--- coverage_badge/__main__.py
+++ coverage_badge/__main__.py
@@ -8,33 +8,34 @@ import os
 import sys
 import argparse
 import pkg_resources
+
 try:
     import coverage
 except ImportError:
     coverage = None
 
 
-__version__ = '1.0.1'
+__version__ = "1.0.1"
 
 
-DEFAULT_COLOR = '#a4a61d'
+DEFAULT_COLOR = "#a4a61d"
 COLORS = {
-    'brightgreen': '#4c1',
-    'green': '#97CA00',
-    'yellowgreen': '#a4a61d',
-    'yellow': '#dfb317',
-    'orange': '#fe7d37',
-    'red': '#e05d44',
-    'lightgrey': '#9f9f9f',
+    "brightgreen": "#4c1",
+    "green": "#97CA00",
+    "yellowgreen": "#a4a61d",
+    "yellow": "#dfb317",
+    "orange": "#fe7d37",
+    "red": "#e05d44",
+    "lightgrey": "#9f9f9f",
 }
 
 COLOR_RANGES = [
-    (95, 'brightgreen'),
-    (90, 'green'),
-    (75, 'yellowgreen'),
-    (60, 'yellow'),
-    (40, 'orange'),
-    (0, 'red'),
+    (95, "brightgreen"),
+    (90, "green"),
+    (75, "yellowgreen"),
+    (60, "yellow"),
+    (40, "orange"),
+    (0, "red"),
 ]
 
 
@@ -42,6 +43,7 @@ class Devnull(object):
     """
     A file like object that does nothing.
     """
+
     def write(self, *args, **kwargs):
         pass
 
@@ -63,6 +65,7 @@ def get_total():
         property.
 
         """
+
         def __init__(self, percent):
             self.percent = percent
 
@@ -80,20 +83,22 @@ def get_color(total):
     try:
         xtotal = int(total)
     except ValueError:
-        return COLORS['lightgrey']
+        return COLORS["lightgrey"]
     for range_, color in COLOR_RANGES:
         if xtotal >= range_:
             return COLORS[color]
 
 
-def get_badge(total, color=DEFAULT_COLOR):
+def get_badge(total, color=DEFAULT_COLOR, name=None):
     """
     Read the SVG template from the package, update total, return SVG as a
     string.
     """
-    template_path = os.path.join('templates', 'flat.svg')
-    template = pkg_resources.resource_string(__name__, template_path).decode('utf8')
-    return template.replace('{{ total }}', total).replace('{{ color }}', color)
+    if name is None:
+        name = "flat.svg"
+    template_path = os.path.join("templates", name)
+    template = pkg_resources.resource_string(__name__, template_path).decode("utf8")
+    return template.replace("{{ total }}", total).replace("{{ color }}", color)
 
 
 def parse_args(argv=None):
@@ -101,16 +106,31 @@ def parse_args(argv=None):
     Parse the command line arguments.
     """
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-o', dest='filepath',
-            help='Save the file to the specified path.')
-    parser.add_argument('-p', dest='plain_color', action='store_true',
-            help='Plain color mode. Standard green badge.')
-    parser.add_argument('-f', dest='force', action='store_true',
-            help='Force overwrite image, use with -o key.')
-    parser.add_argument('-q', dest='quiet', action='store_true',
-            help='Don\'t output any non-error messages.')
-    parser.add_argument('-v', dest='print_version', action='store_true',
-            help='Show version.')
+    parser.add_argument(
+        "-o", dest="filepath", help="Save the file to the specified path."
+    )
+    parser.add_argument(
+        "-p",
+        dest="plain_color",
+        action="store_true",
+        help="Plain color mode. Standard green badge.",
+    )
+    parser.add_argument(
+        "-f",
+        dest="force",
+        action="store_true",
+        help="Force overwrite image, use with -o key.",
+    )
+    parser.add_argument(
+        "-q",
+        dest="quiet",
+        action="store_true",
+        help="Don't output any non-error messages.",
+    )
+    parser.add_argument(
+        "-v", dest="print_version", action="store_true", help="Show version."
+    )
+    parser.add_argument("-n", dest="template_name", help="Template name.")
 
     # If arguments have been passed in, use them.
     if argv:
@@ -126,14 +146,14 @@ def save_badge(badge, filepath, force=False):
     Save badge to the specified path.
     """
     # Validate path (part 1)
-    if filepath.endswith('/'):
-        print('Error: Filepath may not be a directory.')
+    if filepath.endswith("/"):
+        print("Error: Filepath may not be a directory.")
         sys.exit(1)
 
     # Get absolute filepath
     path = os.path.abspath(filepath)
-    if not path.lower().endswith('.svg'):
-        path += '.svg'
+    if not path.lower().endswith(".svg"):
+        path += ".svg"
 
     # Validate path (part 2)
     if not force and os.path.exists(path):
@@ -141,7 +161,7 @@ def save_badge(badge, filepath, force=False):
         sys.exit(1)
 
     # Write file
-    with open(path, 'w') as f:
+    with open(path, "w") as f:
         f.write(badge)
 
     return path
@@ -155,32 +175,32 @@ def main(argv=None):
 
     # Print version
     if args.print_version:
-        print('coverage-badge v{}'.format(__version__))
+        print("coverage-badge v{}".format(__version__))
         sys.exit(0)
 
     # Check for coverage
     if coverage is None:
-        print('Error: Python coverage module not installed.')
+        print("Error: Python coverage module not installed.")
         sys.exit(1)
 
     # Generate badge
     try:
         total = get_total()
     except coverage.misc.CoverageException as e:
-        print('Error: {} Did you run coverage first?'.format(e))
+        print("Error: {} Did you run coverage first?".format(e))
         sys.exit(1)
 
     color = DEFAULT_COLOR if args.plain_color else get_color(total)
-    badge = get_badge(total, color)
+    badge = get_badge(total, color, name=args.template_name)
 
     # Show or save output
     if args.filepath:
         path = save_badge(badge, args.filepath, args.force)
         if not args.quiet:
-            print('Saved badge to {}'.format(path))
+            print("Saved badge to {}".format(path))
     else:
-        print(badge, end='')
+        print(badge, end="")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
